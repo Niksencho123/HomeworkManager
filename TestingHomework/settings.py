8dd2d59957464417d978061721f98624c2da2089
Niksencho123 <132150321+Niksencho123@users.noreply.github.com>
2025-09-02 21:41:13 +0300

    Финален commit преди пускане

--- TestingHomework/settings.py
+++ TestingHomework/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "https://lodge-king-care-murphy.trycloudflare.com"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "https://lodge-king-care-murphy.trycloudflare.com", "https://homework-class9j.onrender.com"]
 
 # Application definition
 
