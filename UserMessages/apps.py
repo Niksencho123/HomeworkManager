835a175e174583df8aa8d9bce621352b5fac8ab0
Niksencho123 <132150321+Niksencho123@users.noreply.github.com>
2025-09-12 21:55:49 +0300

    0.8.2 - Popravka

--- UserMessages/apps.py
+++ UserMessages/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UsermessagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'UserMessages'
+    verbose_name = 'Модул "Ученическа социална мрежа"'
